196f0779adb97a004aaad66c8fd4485707d44f0c
jsing <joosing711@gmail.com>
2022-01-31 18:50:12 +0900

    Init project

---
+++ src/main/java/practice/netty/practicenetty/PracticeNettyApplication.java
@@ -0,0 +1,13 @@
+package practice.netty.practicenetty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticeNettyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticeNettyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/practice/netty/practicenetty/PracticeNettyApplicationTests.java
@@ -0,0 +1,13 @@
+package practice.netty.practicenetty;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeNettyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
