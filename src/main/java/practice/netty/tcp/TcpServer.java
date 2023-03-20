ec214e2d498c167f07c35bc87002e9765da90ffd
Jsing <joosing711@gmail.com>
2023-03-20 21:04:19 +0900

    Separate the default settings from those that can be set on start() in TcpServer

--- src/main/java/practice/netty/tcp/TcpServer.java
+++ src/main/java/practice/netty/tcp/TcpServer.java
@@ -1,10 +1,7 @@
 package practice.netty.tcp;
 
 import io.netty.bootstrap.ServerBootstrap;
-import io.netty.channel.Channel;
-import io.netty.channel.ChannelFuture;
-import io.netty.channel.ChannelInitializer;
-import io.netty.channel.EventLoopGroup;
+import io.netty.channel.*;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
 import io.netty.handler.codec.LineBasedFrameDecoder;
@@ -19,37 +16,40 @@ import java.net.SocketAddress;
 import java.util.concurrent.*;
 
 public class TcpServer implements ClientActiveListener, ReadableQueueListener {
-    ServerBootstrap bootstrap;
-    ConcurrentHashMap<SocketAddress, Channel> activeChannelMap;
-    ConcurrentHashMap<SocketAddress, BlockingQueue<String>> channelReadQueueMap;
+    private final ServerBootstrap bootstrap;
+    private final ConcurrentHashMap<SocketAddress, Channel> activeChannelMap;
+    private final ConcurrentHashMap<SocketAddress, BlockingQueue<String>> channelReadQueueMap;
 
     public TcpServer() {
         bootstrap = new ServerBootstrap();
         activeChannelMap = new ConcurrentHashMap<>();
         channelReadQueueMap = new ConcurrentHashMap<>();
+        EventLoopGroup acceptGroup = new NioEventLoopGroup();
+        EventLoopGroup serviceGroup = new NioEventLoopGroup();
+        ChannelInitializer<Channel> channelInitializer = new ChannelInitializer<>() {
+            @Override
+            protected void initChannel(Channel ch) {
+                ch.pipeline()
+                        // Inbound
+                        .addLast(new ClientActiveNotifier(TcpServer.this))
+                        .addLast(new LineBasedFrameDecoder(1024))
+                        .addLast(new StringDecoder())
+                        .addLast(new ReadDataUpdater(TcpServer.this))
+                        // Outbound
+                        .addLast(new StringEncoder())
+                        .addLast(new LineAppender("\n"));
+            }
+        };
+        bootstrap.group(acceptGroup, serviceGroup)
+                .channel(NioServerSocketChannel.class)
+                .childHandler(channelInitializer);
     }
 
     public Future<Boolean> start(int bindPort) {
+        bootstrap.localAddress("0.0.0.0", bindPort);
         CompletableFuture<Boolean> startFuture = new CompletableFuture<>();
-        EventLoopGroup acceptGroup = new NioEventLoopGroup();
-        EventLoopGroup serviceGroup = new NioEventLoopGroup();
-        bootstrap.group(acceptGroup, serviceGroup)
-                .channel(NioServerSocketChannel.class)
-                .localAddress("0.0.0.0", bindPort)
-                .childHandler(new ChannelInitializer<>() {
-                    @Override
-                    protected void initChannel(Channel ch) {
-                        ch.pipeline()
-                                // Inbound
-                                .addLast(new ClientActiveNotifier(TcpServer.this))
-                                .addLast(new LineBasedFrameDecoder(1024))
-                                .addLast(new StringDecoder())
-                                .addLast(new ReadDataUpdater(TcpServer.this))
-                                // Outbound
-                                .addLast(new StringEncoder())
-                                .addLast(new LineAppender("\n"));
-                    }
-                }).bind().addListener((ChannelFuture future) -> startFuture.complete(future.isSuccess()));
+        ChannelFutureListener bindFutureListener = (ChannelFuture future) -> startFuture.complete(future.isSuccess());
+        bootstrap.bind().addListener(bindFutureListener);
         return startFuture;
     }
 
