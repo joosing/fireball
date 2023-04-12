d804a9e6044a5898bb60970a3140e904f2400488
Jsing <joosing711@gmail.com>
2023-04-12 12:05:18 +0900

    Remove the feature toggle that chooses how to respond to file patch request, and instead always use FileRegion

--- src/main/java/practice/netty/handler/inbound/FileResponsor.java
+++ src/main/java/practice/netty/handler/inbound/FileResponsor.java
@@ -17,7 +17,6 @@ import java.util.Optional;
 public class FileResponsor extends SimpleChannelInboundHandler<Message> {
     private final String rootPath;
     private final Map<Class<? extends Message>, ResponseFunction> responseFunctionMap;
-    private static final boolean fileRegionFeature = true; // Feature Toggle (FileRegion 사용 여부)
 
     public FileResponsor(String rootPath) {
         this.rootPath = rootPath;
@@ -36,21 +35,13 @@ public class FileResponsor extends SimpleChannelInboundHandler<Message> {
                 .ifPresent(response -> ctx.writeAndFlush(response));
     }
 
-    private Optional<Message> responseFileFetchRequest(Message request, ByteBufAllocator allocator) throws IOException {
-        if (fileRegionFeature) {
-            return implementFileRegionFeature(request, allocator);
-        } else {
-            return implementDirectReadFeature(request, allocator);
-        }
-    }
-
     /**
      * 파일 패치 요청에 FileRegion 매커니즘을 활용해 응답합니다. (zero-copy)
      * @param request 파일 패치 요청
      * @param allocator ByteBuf 할당자
      * @return FileRegion을 담은 응답
      */
-    private Optional<Message> implementFileRegionFeature(Message request, ByteBufAllocator allocator) {
+    private Optional<Message> responseFileFetchRequest(Message request, ByteBufAllocator allocator) {
         var fileFetchRequest = (FileFetchRequest) request;
         var actualPath = rootPath + fileFetchRequest.getRemoteFilePath();
         var response = FileFetchRegionResponse.builder()
@@ -61,12 +52,13 @@ public class FileResponsor extends SimpleChannelInboundHandler<Message> {
 
     /**
      * 파일 패치 요청에 직접 파일을 읽어서 응답합니다. 이 메서드는 Deprecated 되었습니다. 직접 파일을 읽어서 전송하는 것 보다 FileRegion을
-     * 활용해서 전송하는 것이 대용량 파일 전송 시 더 좋은 성능을 보입니다.
+     * 활용해서 전송하는 것이 대용량 파일 전송 시 더 적은 메모리를 사용하고, 속도도 더 빠릅니다.
      * @param request 파일 패치 요청
      * @param allocator ByteBuf 할당자
      * @return 파일 컨텐츠를 담은 응답
      */
-    private Optional<Message> implementDirectReadFeature(Message request, ByteBufAllocator allocator) throws IOException {
+    @Deprecated
+    private Optional<Message> responseFileFetchRequestByDirect(Message request, ByteBufAllocator allocator) throws IOException {
         var fileFetchRequest = (FileFetchRequest) request;
         var path = rootPath + fileFetchRequest.getRemoteFilePath();
         var contents = NettyFileUtils.readAllBytes(path, allocator);
