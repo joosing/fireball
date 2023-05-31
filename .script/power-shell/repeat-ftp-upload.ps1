530eefae312319db1ed146a8275b4e04a7abf251
Jsing <joosing711@gmail.com>
2023-05-31 18:59:01 +0900

    Add comment logging in the repeat-ftp-upload.ps1 script

--- .script/power-shell/repeat-ftp-upload.ps1
+++ .script/power-shell/repeat-ftp-upload.ps1
@@ -1,11 +1,12 @@
 $ftpSession = New-Object -TypeName System.Net.WebClient
 $ftpSession.Credentials = New-Object System.Net.NetworkCredential({id}, {password})
 $executionTimes = @()
-for ($i = 1; $i -le 10; $i++) {
+for ($i = 1; $i -le 50; $i++) {
     $result = Measure-Command {
         $ftpSession.UploadFile("ftp://localhost/remote-ftp.dat", "c:/Temp/local.dat")
     } | Select-Object -ExpandProperty TotalMilliseconds
 	Start-Sleep -Milliseconds 100
+	Write-Host "$i execution time: $result ms"
     $executionTimes += $result
 }
 $averageTime = ($executionTimes | Measure-Object -Average).Average
