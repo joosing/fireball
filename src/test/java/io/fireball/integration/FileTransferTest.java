98aea99e473be88c02c36c49ddab66565d170aa0
Jsing <joosing711@gmail.com>
2023-06-01 15:33:36 +0900

    Have the test profile load when testing

--- src/test/java/io/fireball/integration/FileTransferTest.java
+++ src/test/java/io/fireball/integration/FileTransferTest.java
@@ -9,6 +9,7 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ActiveProfiles;
 
 import java.nio.file.Path;
 import java.time.LocalDateTime;
@@ -17,6 +18,7 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 
 @Slf4j
+@ActiveProfiles("test")
 public class FileTransferTest extends RestAssuredTest {
     @Autowired
     ChannelSpecProvider channelSpecProvider;
