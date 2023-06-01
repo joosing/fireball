875b6ae0e4895dcc51b91fbfcafa2352e945dcc6
Jsing <joosing711@gmail.com>
2023-06-01 17:01:40 +0900

    Add a REST API for ftp file upload test

---
+++ .locust/ftp-file-upload.py
@@ -0,0 +1,23 @@
+import uuid
+from locust import HttpUser, task, between
+
+
+class FireballClientUser(HttpUser):
+    host = "http://localhost:8080"
+    wait_time = between(1, 5)
+
+    @task
+    def upload_file(self):
+        # remote_file_path = str(uuid.uuid4()) + ".dat"
+        remote_file_path = "remote-1000.dat"
+        body = {
+            "local": {
+                "filePath": "local-1000.dat"
+            },
+            "remote": {
+                "ip": "127.0.0.1",
+                "port": 21,
+                "filePath": remote_file_path
+            }
+        }
+        self.client.post("/test/ftp/upload", json=body)
---
+++ src/main/java/io/fireball/controller/FtpFileClientController.java
@@ -0,0 +1,48 @@
+package io.fireball.controller;
+
+import io.fireball.dto.FileTransferDto;
+import io.fireball.specification.channel.FileClientSpec;
+import lombok.RequiredArgsConstructor;
+import org.apache.commons.net.ftp.FTPClient;
+import org.apache.commons.net.ftp.FTPReply;
+import org.springframework.context.annotation.Profile;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.nio.file.Path;
+
+@Profile("test")
+@RestController
+@RequestMapping("/test/ftp")
+@RequiredArgsConstructor
+public class FtpFileClientController {
+    private final FileClientSpec fileClientSpec;
+
+    @PostMapping("/upload")
+    public ResponseEntity<Boolean> uploadFile(@RequestBody FileTransferDto fileTransferDto) throws Exception {
+        // create
+        var ftp = new FTPClient();
+
+        // connect
+        ftp.connect(fileTransferDto.getRemote().getIp(), fileTransferDto.getRemote().getPort());
+        int reply = ftp.getReplyCode();
+        if (!FTPReply.isPositiveCompletion(reply)) {
+            ftp.disconnect();
+            throw new IOException("Exception in connecting to FTP Server");
+        }
+        // login
+        ftp.login("test", "1234");
+
+        // normalize path
+        var srcPath = Path.of(fileClientSpec.rootPath(), fileTransferDto.getLocal().getFilePath()).normalize().toString();
+
+        // upload file
+        var result = ftp.storeFile(fileTransferDto.getRemote().getFilePath(), new FileInputStream(srcPath));
+        return ResponseEntity.ok(result);
+    }
+}
