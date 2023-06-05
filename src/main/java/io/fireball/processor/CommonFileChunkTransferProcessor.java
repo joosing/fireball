ec2e2bb6f170a574c63925edfb24b6980a7ea4d6
Jsing <joosing711@gmail.com>
2023-06-05 16:16:16 +0900

    Handle exceptions where the requested file is a directory

--- src/main/java/io/fireball/processor/CommonFileChunkTransferProcessor.java
+++ src/main/java/io/fireball/processor/CommonFileChunkTransferProcessor.java
@@ -1,5 +1,6 @@
 package io.fireball.processor;
 
+import io.fireball.exception.NotFileException;
 import io.fireball.message.ChunkType;
 import io.fireball.message.OutboundFileChunk;
 import io.fireball.message.ProtocolMessage;
@@ -13,34 +14,42 @@ import java.util.List;
 
 public class CommonFileChunkTransferProcessor implements FileTransferProcessor {
 
-    @Override
-    public List<ProtocolMessage> process(String srcPath, String dstPath, int chunkSize) throws FileNotFoundException {
-        // Check if the source file exists
+    private static void validate(String srcPath) throws Exception {
+        if (Files.isDirectory(Path.of(srcPath))) {
+            throw new NotFileException(srcPath);
+        }
         if (!Files.exists(Path.of(srcPath))) {
             throw new FileNotFoundException(srcPath);
         }
+    }
 
-        // Send an empty chunk to indicate the start of the file
-        var result = new ArrayList<ProtocolMessage>();
+    @Override
+    public List<ProtocolMessage> process(String srcPath, String dstPath, int chunkSize) throws Exception {
+        var messages = new ArrayList<ProtocolMessage>();
+
+        // Validate
+        validate(srcPath);
+
+        // Start of file
         var startChunk = new OutboundFileChunk(ChunkType.START_OF_FILE, srcPath, dstPath, 0, 0);
-        result.add(startChunk);
+        messages.add(startChunk);
 
-        // Send a file content in chunks
+        // Content in chunks
         long start = 0;
         var srcFile = new File(srcPath);
         long remainBytes = srcFile.length();
         while (remainBytes > 0) {
             var readBytes = (int) Math.min(remainBytes, chunkSize);
             var chunk = new OutboundFileChunk(ChunkType.FILE_CONTENTS, srcPath, dstPath, start, readBytes);
-            result.add(chunk);
+            messages.add(chunk);
             remainBytes -= readBytes;
             start += readBytes;
         }
 
-        // Send an empty chunk to indicate the end of the file
+        // End of file
         var lastChunk = new OutboundFileChunk(ChunkType.END_OF_FILE, srcPath, dstPath, start, 0);
-        result.add(lastChunk);
+        messages.add(lastChunk);
 
-        return result;
+        return messages;
     }
 }
